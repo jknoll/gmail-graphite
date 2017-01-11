0a5b77d18971e231a4434e2fa706b85e97942f29
Justin Knoll <justin.knoll@gmail.com>
2017-01-10 21:06:09 -0800

    Fixed some PEP8 tab violations and added a travis.yml

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 try:
-  from setuptools import setup
+    from setuptools import setup
 except ImportError:
-  from distutils.core import setup
+    from distutils.core import setup
 
 config = {
-  'description': 'Tracks GMail inbox unread message count via RSS and Graphite',
-  'author': 'Justin Knoll',
-  'url': 'https://github.com/jknoll/gmail-graphite',
-  'download_url': 'https://github.com/jknoll/gmail-graphite',
-  'author_email': 'justin.knoll@gmail.com',
-  'version': '0.1',
-  'install_requires': ['nose', 'feedparser'],
-  'packages': ['gmail_graphite'],
-  'scripts': [],
-  'name': 'gmail_graphite'
+    'description': 'Track GMail unread message count via RSS and Graphite',
+    'author': 'Justin Knoll',
+    'url': 'https://github.com/jknoll/gmail-graphite',
+    'download_url': 'https://github.com/jknoll/gmail-graphite',
+    'author_email': 'justin.knoll@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose', 'feedparser'],
+    'packages': ['gmail_graphite'],
+    'scripts': [],
+    'name': 'gmail_graphite'
 }
 
 setup(**config)
